c80af16bef6689169825715b9e246de0044dccaf
Gr8fulnez <oluwolejohn72@gmail.com>
2022-06-18 00:53:04 +0100

    initializing repository

---
+++ People/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.People
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public bool HasPet;
+        public void Greeting()
+        {
+            Console.WriteLine("Hello World, My name is {0}, I am {1} years old, my pet status is {2}", name, age, HasPet);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.People;
+
+namespace Test
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            
+            //Variable for person 1
+            p.age= 10;
+            p.name= "Adewale";
+            p.HasPet = true;
+           
+            p.Greeting();
+            
+            Console.ReadKey();
+        }
+    }
+}
